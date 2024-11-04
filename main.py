5087524c423f865f9b0acb305eb5226731dac3ba
jumplight100 <ivan.fateev.nz@gmail.com>
2024-11-04 16:58:57 +1300

    Fixed main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from PIL import Image, ImageOps
-# load and resize the image to 100 by x with thumbnail doing the reize calc
-size = [100,50]
+# input the image and resize it
+new_width = 50
 inputname = input("name of image inc .jpg or .png:")
 with Image.open(inputname) as im:
-    
-    im.thumbnail(size,Image.LANCZOS)
+    width, height = im.size
+    new_height = int(height/width * new_width *0.55)
+    im = im.resize((new_width, new_height))
     
     #convert to grayscale
     im = ImageOps.grayscale(im)
-    width, height = im.size
+    
 
     #ascii define white to black
-    ascii_chars = "#@%*+=-:. "
+    ascii_chars = "@%#*+=-:. "
     
     #opens the text file
     f = open(inputname[0:-4]+".txt","w")
 
     #goes thorugh each pixel
-    for i in range(height):
+    for i in range(new_height):
         f.write("\n")
-        for j in range(width):
+        for j in range(new_width):
 
             #gets pixel value
             pixel = im.getpixel((j,i))
 
             #make each pixel an ascii char 0(black)-255(white)
             f.write(ascii_chars[round(pixel/255*len(ascii_chars))])
+    print("Outputted ascii text file")
