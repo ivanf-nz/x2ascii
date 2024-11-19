11fd5e5a9f944f80bcbcc38d4112ca219f98bd6a
jumplight100 <ivan.fateev.nz@gmail.com>
2024-11-19 19:38:28 +1300

    Fixed 255 value and tweaked converting the size

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from PIL import Image, ImageOps
 # input the image and resize it
-new_width = 50
+new_width = 57
 inputname = input("name of image inc .jpg or .png:")
 with Image.open(inputname) as im:
     width, height = im.size
-    new_height = int(height/width * new_width *0.55)
+    new_height = int(height/width * new_width *0.5)
     im = im.resize((new_width, new_height))
     
     #convert to grayscale
     im = ImageOps.grayscale(im)
-    
+
 
     #ascii define white to black
     ascii_chars = "@%#*+=-:. "
-    
+
     #opens the text file
     f = open(inputname[0:-4]+".txt","w")
 
@@ -26,5 +26,5 @@ with Image.open(inputname) as im:
             pixel = im.getpixel((j,i))
 
             #make each pixel an ascii char 0(black)-255(white)
-            f.write(ascii_chars[round(pixel/255*len(ascii_chars))])
+            f.write(ascii_chars[int(pixel/255*(len(ascii_chars)-1))])
     print("Outputted ascii text file")
